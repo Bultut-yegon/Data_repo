f821a7893e4d072bd175a440a6a4cfb8002eec73
Bultut-yegon <bultut9785@gmail.com>
2024-05-20 20:59:52 +0300

    demo

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,2 +1,4 @@
 print("Hello user",'Welcome ','to our system' , sep='-',end='*''\n')
-print('Hello',end='')
+print('Hello',end='')#sep and end keywords in print() function
+#end prints what is at the end after print function is executed
+#sep keyword specifies the separator used after each word
